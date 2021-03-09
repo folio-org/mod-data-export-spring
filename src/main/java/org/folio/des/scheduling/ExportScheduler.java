494e887d53caeb7b6981593e35efceedf5f974d7
Roman Leshchenko <Roman_Leshchenko@epam.com>
2021-03-09 19:37:34 +0200

    MODEXPS-4 Added FolioContext initialization on tenant register step

--- src/main/java/org/folio/des/scheduling/ExportScheduler.java
+++ src/main/java/org/folio/des/scheduling/ExportScheduler.java
@@ -1,26 +1,19 @@
 package org.folio.des.scheduling;
 
+import java.util.Optional;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.folio.des.client.AuthClient;
-import org.folio.des.config.FolioExecutionContextHelper;
-import org.folio.des.domain.dto.AuthCredentials;
 import org.folio.des.domain.dto.ExportConfig;
 import org.folio.des.domain.dto.Job;
+import org.folio.des.security.AuthtService;
 import org.folio.des.service.ExportConfigService;
 import org.folio.des.service.JobService;
-import org.folio.spring.integration.XOkapiHeaders;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Component;
-
-import java.util.*;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 @Log4j2
 @Component
@@ -31,42 +24,9 @@ public class ExportScheduler implements SchedulingConfigurer {
   private ScheduledTaskRegistrar registrar;
   private Job scheduledJob;
   private final ExportTrigger trigger;
-  private final AuthClient authClient;
   private final JobService jobService;
   private final ExportConfigService configService;
-  private final FolioExecutionContextHelper contextHelper;
-
-  @Value("${folio.tenant.name}")
-  private String tenant;
-  @Value("${folio.tenant.password}")
-  private String password;
-  @Value("${folio.tenant.username}")
-  private String username;
-  @Value("${folio.okapi.url}")
-  private String okapiUrl;
-
-  @Override
-  public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-    registrar = taskRegistrar;
-    initConfiguration();
-    taskRegistrar.setScheduler(taskExecutor());
-    log.info("Scheduled {}.", scheduledJob);
-    taskRegistrar.addTriggerTask(() -> jobService.upsert(scheduledJob), trigger);
-  }
-
-  public void updateTasks(ExportConfig exportConfig) {
-    trigger.setConfig(exportConfig);
-    if (registrar.hasTasks()) {
-      registrar.destroy();
-      registrar.afterPropertiesSet();
-    }
-  }
-
-  private void initConfiguration() {
-    authorizeWithToken();
-
-    fetchConfiguration();
-  }
+  private final AuthtService authtService;
 
   private void fetchConfiguration() {
     Optional<ExportConfig> savedConfig = configService.getConfig();
@@ -74,30 +34,40 @@ public class ExportScheduler implements SchedulingConfigurer {
     savedConfig.ifPresent(exportConfig -> scheduledJob = defaultJob(exportConfig));
   }
 
-  private void authorizeWithToken() {
-    AuthCredentials authDto = createCredentials();
-
-    ResponseEntity<String> authResponse = authClient.getApiKey(tenant, authDto);
-    HttpHeaders headers = authResponse.getHeaders();
-    List<String> token = headers.get(XOkapiHeaders.TOKEN);
-
-    Map<String, Collection<String>> okapiHeaders = new HashMap<>();
-    okapiHeaders.put(XOkapiHeaders.TOKEN, token);
-    okapiHeaders.put(XOkapiHeaders.TENANT, List.of(tenant));
-    okapiHeaders.put(XOkapiHeaders.URL, List.of(okapiUrl));
-
-    contextHelper.init(okapiHeaders);
-  }
-
-  private AuthCredentials createCredentials() {
-    AuthCredentials authDto = new AuthCredentials();
-    authDto.setPassword(password);
-    authDto.setUsername(username);
-    return authDto;
-  }
-
   private Executor taskExecutor() {
     return Executors.newScheduledThreadPool(100);
+  }
+
+  @Override
+  public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+    registrar = taskRegistrar;
+    taskRegistrar.setScheduler(taskExecutor());
+
+    taskRegistrar.addTriggerTask(
+        () -> {
+          if (authtService.isTenantRegistered()) {
+            authtService.initializeFolioScope();
+            jobService.upsert(scheduledJob);
+          }
+        },
+        trigger);
+  }
+
+  public void initScheduleConfiguration() {
+    fetchConfiguration();
+    reconfigureSchedule();
+  }
+
+  public void updateTasks(ExportConfig exportConfig) {
+    trigger.setConfig(exportConfig);
+    reconfigureSchedule();
+  }
+
+  private void reconfigureSchedule() {
+    if (registrar.hasTasks()) {
+      registrar.destroy();
+      registrar.afterPropertiesSet();
+    }
   }
 
   private Job defaultJob(ExportConfig exportConfig) {
@@ -114,5 +84,4 @@ public class ExportScheduler implements SchedulingConfigurer {
 
     return job;
   }
-
 }
