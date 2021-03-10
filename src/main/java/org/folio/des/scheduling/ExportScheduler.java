ad8c9b37b2740c60db96da0af7499b8d9906c8f8
Aleksei Prokhorov <Aleksei_Prokhorov@epam.com>
2021-03-10 14:00:00 +0300

    MODEXPS-4 Added FolioContext initialization on tenant register step Scheduler fixed after context changes.

--- src/main/java/org/folio/des/scheduling/ExportScheduler.java
+++ src/main/java/org/folio/des/scheduling/ExportScheduler.java
@@ -1,8 +1,5 @@
 package org.folio.des.scheduling;
 
-import java.util.Optional;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.des.domain.dto.ExportConfig;
@@ -15,41 +12,42 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+import java.util.concurrent.Executors;
+
 @Log4j2
 @Component
 @EnableScheduling
 @RequiredArgsConstructor
 public class ExportScheduler implements SchedulingConfigurer {
 
-  private ScheduledTaskRegistrar registrar;
-  private Job scheduledJob;
   private final ExportTrigger trigger;
   private final JobService jobService;
   private final ExportConfigService configService;
   private final AuthService authService;
 
+  private ScheduledTaskRegistrar registrar;
+  private Job scheduledJob;
+
   @Override
   public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
     registrar = taskRegistrar;
-    taskRegistrar.setScheduler(taskExecutor());
-    log.info("Scheduled {}.", scheduledJob);
-    taskRegistrar.addTriggerTask(
-        () -> {
-          if (authService.isTenantRegistered()) {
-            authService.initializeFolioScope();
-            jobService.upsert(scheduledJob);
-          }
-        },
-        trigger);
+    taskRegistrar.setScheduler(Executors.newScheduledThreadPool(100));
+    taskRegistrar.addTriggerTask(() -> {
+      if (authService.isTenantRegistered()) {
+        authService.initializeFolioScope();
+        jobService.upsert(scheduledJob);
+      }
+    }, trigger);
   }
 
   public void initScheduleConfiguration() {
-    fetchConfiguration();
-    reconfigureSchedule();
+    updateTasks(fetchConfiguration());
   }
 
   public void updateTasks(ExportConfig exportConfig) {
     trigger.setConfig(exportConfig);
+    createScheduledJob(exportConfig);
     reconfigureSchedule();
   }
 
@@ -60,34 +58,27 @@ public class ExportScheduler implements SchedulingConfigurer {
     }
   }
 
-  private Job defaultJob(ExportConfig exportConfig) {
-    Job job = new Job();
-    job.setType(exportConfig.getType());
-    job.setIsSystemSource(true);
-
-    var exportTypeSpecificParameters = exportConfig.getExportTypeSpecificParameters();
-    if (exportTypeSpecificParameters == null) {
-      log.error("There is no configuration for scheduled job");
-      return job;
+  private void createScheduledJob(ExportConfig exportConfig) {
+    if (exportConfig == null) {
+      scheduledJob = null;
+    } else {
+      scheduledJob = new Job();
+      scheduledJob.setType(exportConfig.getType());
+      scheduledJob.setIsSystemSource(true);
+      scheduledJob.setExportTypeSpecificParameters(exportConfig.getExportTypeSpecificParameters());
     }
-    job.setExportTypeSpecificParameters(exportTypeSpecificParameters);
-
-    return job;
+    log.info("Scheduled job assigned {}.", scheduledJob);
   }
 
-  private void fetchConfiguration() {
+  private ExportConfig fetchConfiguration() {
     Optional<ExportConfig> savedConfig = configService.getConfig();
     if (savedConfig.isPresent()) {
       log.info("Got {}.", savedConfig.get());
-      trigger.setConfig(savedConfig.get());
-      scheduledJob = defaultJob(savedConfig.get());
+      return savedConfig.get();
     } else {
       log.info("No export schedules found.");
+      return null;
     }
-  }
-
-  private Executor taskExecutor() {
-    return Executors.newScheduledThreadPool(100);
   }
 
 }
