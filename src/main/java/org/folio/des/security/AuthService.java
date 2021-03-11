16fc3a55762793f5efbba1b47732fb6194c29d4b
Aleksei Prokhorov <Aleksei_Prokhorov@epam.com>
2021-03-11 14:56:37 +0300

    MODEXPS-4 Added FolioContext initialization on tenant register step Context fixed after changes.

--- src/main/java/org/folio/des/security/AuthService.java
+++ src/main/java/org/folio/des/security/AuthService.java
@@ -2,6 +2,8 @@ package org.folio.des.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.folio.des.client.AuthClient;
 import org.folio.des.domain.dto.AuthCredentials;
 import org.folio.spring.integration.XOkapiHeaders;
@@ -29,9 +31,14 @@ public class AuthService {
     credentials.setPassword(password);
 
     ResponseEntity<String> authResponse = authClient.getApiKey(tenant, url, credentials);
-    log.info("Logged in as {}.", username);
+    List<String> result = authResponse.getHeaders().get(XOkapiHeaders.TOKEN);
+    if (CollectionUtils.isNotEmpty(result) && StringUtils.isNotBlank(result.get(0))) {
+      log.info("Logged in as {}.", username);
+    } else {
+      log.error("Can't get token logging in as {}.", username);
+    }
 
-    return authResponse.getHeaders().get(XOkapiHeaders.TOKEN);
+    return result;
   }
 
 }
