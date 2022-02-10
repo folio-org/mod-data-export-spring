e77dd210cc2788d1bdcde1c8aa1d530f25f409e3
Andrei <40025074+AndreiMPV@users.noreply.github.com>
2022-02-10 11:18:23 +0300

    [MODEXPS-54] - Update time zone approach (#152)
    * [MODEXPS-54] - Update time zone approach

--- src/main/java/org/folio/des/converter/aqcuisition/EdifactOrdersExportConfigToTaskTriggerConverter.java
+++ src/main/java/org/folio/des/converter/aqcuisition/EdifactOrdersExportConfigToTaskTriggerConverter.java
@@ -1,19 +1,16 @@
 package org.folio.des.converter.aqcuisition;
 
+import static org.folio.des.domain.dto.ScheduleParameters.SchedulePeriodEnum.NONE;
+
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.folio.des.client.ConfigurationClient;
-import org.folio.des.domain.dto.ConfigurationCollection;
 import org.folio.des.domain.dto.ExportConfig;
 import org.folio.des.domain.dto.ExportTypeSpecificParameters;
-import org.folio.des.domain.dto.ModelConfiguration;
 import org.folio.des.domain.dto.ScheduleParameters;
 import org.folio.des.domain.dto.VendorEdiOrdersExportConfig;
-import org.folio.des.domain.dto.acquisition.OrgConfig;
 import org.folio.des.scheduling.acquisition.AcqBaseExportTaskTrigger;
 import org.folio.des.scheduling.base.ExportTaskTrigger;
 import org.folio.des.validator.acquisition.EdifactOrdersExportParametersValidator;
@@ -22,9 +19,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -32,51 +26,28 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @Service
 public class EdifactOrdersExportConfigToTaskTriggerConverter implements Converter<ExportConfig, List<ExportTaskTrigger>>  {
-  private static final String CONFIG_QUERY = "module==ORG and configName==localeSettings";
-  public static final String TZ_UTC = "UTC";
-
   private EdifactOrdersExportParametersValidator validator;
-  private ConfigurationClient configurationClient;
-  private final ObjectMapper objectMapper;
 
   @Override
   public List<ExportTaskTrigger> convert(ExportConfig exportConfig) {
     ExportTypeSpecificParameters specificParameters = exportConfig.getExportTypeSpecificParameters();
     Errors errors = new BeanPropertyBindingResult(specificParameters, "specificParameters");
     validator.validate(specificParameters, errors);
-    if (ExportConfig.SchedulePeriodEnum.NONE != exportConfig.getSchedulePeriod()) {
-      List<ExportTaskTrigger> exportTaskTriggers = new ArrayList<>();
-      Optional.ofNullable(specificParameters.getVendorEdiOrdersExportConfig())
-              .map(VendorEdiOrdersExportConfig::getEdiSchedule)
-              .ifPresent(ediSchedule -> {
-                ScheduleParameters scheduleParameters = ediSchedule.getScheduleParameters();
-                if (ediSchedule.getScheduleParameters() != null ) {
-                  if (scheduleParameters.getId() == null) {
-                    scheduleParameters.setId(UUID.randomUUID());
-                  }
-                  String timeZone = getSystemTimeZone();
-                  scheduleParameters.setTimeZone(timeZone);
-                  exportTaskTriggers.add(new AcqBaseExportTaskTrigger(scheduleParameters, ediSchedule.getEnableScheduledExport()));
+
+    List<ExportTaskTrigger> exportTaskTriggers = new ArrayList<>(1);
+    Optional.ofNullable(specificParameters.getVendorEdiOrdersExportConfig())
+            .map(VendorEdiOrdersExportConfig::getEdiSchedule)
+            .ifPresent(ediSchedule -> {
+              ScheduleParameters scheduleParameters = ediSchedule.getScheduleParameters();
+              if (scheduleParameters != null && !NONE.equals(scheduleParameters.getSchedulePeriod())) {
+                if (scheduleParameters.getId() == null) {
+                  scheduleParameters.setId(UUID.randomUUID());
                 }
-              });
-      return exportTaskTriggers;
-    }
-    return Collections.emptyList();
-  }
-
-  public String getSystemTimeZone() {
-    ConfigurationCollection configurationCollection = configurationClient.getConfigurations(CONFIG_QUERY);
-    Optional<ModelConfiguration> optConfig = configurationCollection.getConfigs().stream().findFirst();
-
-    if (optConfig.isPresent() && optConfig.get().getValue() != null) {
-      ModelConfiguration config = optConfig.get();
-      try {
-        var orgConfig = objectMapper.readValue(config.getValue(), OrgConfig.class);
-        return Optional.ofNullable(orgConfig.getTimezone()).orElse(TZ_UTC);
-      } catch (JsonProcessingException e) {
-        return TZ_UTC;
-      }
-    }
-    return TZ_UTC;
+                scheduleParameters.setTimeZone(scheduleParameters.getTimeZone());
+                var trigger = new AcqBaseExportTaskTrigger(scheduleParameters, ediSchedule.getEnableScheduledExport());
+                exportTaskTriggers.add(trigger);
+              }
+            });
+    return exportTaskTriggers;
   }
 }
