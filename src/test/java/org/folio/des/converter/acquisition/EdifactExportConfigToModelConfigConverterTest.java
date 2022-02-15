45deab0f53159d7133f15bc67e97d67e217fd575
Andrei <40025074+AndreiMPV@users.noreply.github.com>
2022-02-15 18:33:17 +0300

    [MODEXPS-71] - Scheduled parameters UUID is not saved in the DB (#153)
    * [MODEXPS-71] - Scheduled parameters UUID is not saved in the DB
    
    * [MODEXPS-71] - Back flag for check module register

--- src/test/java/org/folio/des/converter/acquisition/EdifactExportConfigToModelConfigConverterTest.java
+++ src/test/java/org/folio/des/converter/acquisition/EdifactExportConfigToModelConfigConverterTest.java
@@ -7,13 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.folio.des.config.JacksonConfiguration;
 import org.folio.des.converter.aqcuisition.EdifactExportConfigToModelConfigConverter;
 import org.folio.des.domain.dto.BursarFeeFines;
+import org.folio.des.domain.dto.EdiSchedule;
 import org.folio.des.domain.dto.ExportConfig;
 import org.folio.des.domain.dto.ExportType;
 import org.folio.des.domain.dto.ExportTypeSpecificParameters;
 import org.folio.des.domain.dto.ModelConfiguration;
+import org.folio.des.domain.dto.ScheduleParameters;
 import org.folio.des.domain.dto.VendorEdiOrdersExportConfig;
 import org.folio.des.validator.acquisition.EdifactOrdersExportParametersValidator;
 import org.folio.des.validator.acquisition.EdifactOrdersScheduledParamsValidator;
@@ -28,10 +32,13 @@ import org.springframework.boot.test.context.SpringBootTest;
                             EdifactOrdersExportParametersValidator.class, EdifactOrdersScheduledParamsValidator.class})
 class EdifactExportConfigToModelConfigConverterTest {
   @Autowired
-  EdifactExportConfigToModelConfigConverter converter;
+  private EdifactExportConfigToModelConfigConverter converter;
+
+  @Autowired
+  private ObjectMapper objectMapper;
 
   @Test
-  void testConverterIfExportConfigIsValid() {
+  void testConverterIfExportConfigIsValidAndScheduledIdIsNotProvided() throws JsonProcessingException {
     String expId = UUID.randomUUID().toString();
     UUID vendorId = UUID.randomUUID();
     ExportConfig ediConfig = new ExportConfig();
@@ -40,17 +47,68 @@ class EdifactExportConfigToModelConfigConverterTest {
     ExportTypeSpecificParameters parameters = new ExportTypeSpecificParameters();
     VendorEdiOrdersExportConfig vendorEdiOrdersExportConfig = new VendorEdiOrdersExportConfig();
     vendorEdiOrdersExportConfig.setVendorId(vendorId);
+
+    EdiSchedule accountEdiSchedule = new EdiSchedule();
+    accountEdiSchedule.enableScheduledExport(true);
+    String accTime = "17:08:39";
+    ScheduleParameters accScheduledParameters = new ScheduleParameters();
+    accScheduledParameters.setSchedulePeriod(ScheduleParameters.SchedulePeriodEnum.WEEK);
+    accScheduledParameters.setScheduleFrequency(7);
+    accScheduledParameters.setScheduleTime(accTime);
+    accScheduledParameters.setTimeZone("Pacific/Midway");
+    accountEdiSchedule.setScheduleParameters(accScheduledParameters);
+    vendorEdiOrdersExportConfig.setEdiSchedule(accountEdiSchedule);
+
     parameters.setVendorEdiOrdersExportConfig(vendorEdiOrdersExportConfig);
     ediConfig.exportTypeSpecificParameters(parameters);
 
     ModelConfiguration actConfig = converter.convert(ediConfig);
-
+    var actExportConfig = objectMapper.readValue(actConfig.getValue(), ExportConfig.class);
     Assertions.assertAll(
       () -> assertEquals(expId, actConfig.getId()),
       () -> assertEquals(ExportType.EDIFACT_ORDERS_EXPORT + "_" + vendorId + "_" + expId, actConfig.getConfigName()),
       () -> assertEquals(DEFAULT_MODULE_NAME, actConfig.getModule()),
       () -> assertEquals(true, actConfig.getDefault()),
-      () -> assertEquals(true, actConfig.getEnabled())
+      () -> assertEquals(true, actConfig.getEnabled()),
+      () -> assertEquals(expId, actExportConfig.getId())
+    );
+  }
+
+  @Test
+  void testConverterIfExportConfigIsValidAndScheduledIdIsProvidedButNotEqualToExportId() throws JsonProcessingException {
+    String expId = UUID.randomUUID().toString();
+    UUID vendorId = UUID.randomUUID();
+    ExportConfig ediConfig = new ExportConfig();
+    ediConfig.setId(expId);
+    ediConfig.setType(ExportType.EDIFACT_ORDERS_EXPORT);
+    ExportTypeSpecificParameters parameters = new ExportTypeSpecificParameters();
+    VendorEdiOrdersExportConfig vendorEdiOrdersExportConfig = new VendorEdiOrdersExportConfig();
+    vendorEdiOrdersExportConfig.setVendorId(vendorId);
+
+    EdiSchedule accountEdiSchedule = new EdiSchedule();
+    accountEdiSchedule.enableScheduledExport(true);
+    String accTime = "17:08:39";
+    ScheduleParameters accScheduledParameters = new ScheduleParameters();
+    accScheduledParameters.setId(UUID.randomUUID());
+    accScheduledParameters.setSchedulePeriod(ScheduleParameters.SchedulePeriodEnum.WEEK);
+    accScheduledParameters.setScheduleFrequency(7);
+    accScheduledParameters.setScheduleTime(accTime);
+    accScheduledParameters.setTimeZone("Pacific/Midway");
+    accountEdiSchedule.scheduleParameters(accScheduledParameters);
+    vendorEdiOrdersExportConfig.setEdiSchedule(accountEdiSchedule);
+
+    parameters.setVendorEdiOrdersExportConfig(vendorEdiOrdersExportConfig);
+    ediConfig.exportTypeSpecificParameters(parameters);
+
+    ModelConfiguration actConfig = converter.convert(ediConfig);
+    var actExportConfig = objectMapper.readValue(actConfig.getValue(), ExportConfig.class);
+    Assertions.assertAll(
+      () -> assertEquals(expId, actConfig.getId()),
+      () -> assertEquals(ExportType.EDIFACT_ORDERS_EXPORT + "_" + vendorId + "_" + expId, actConfig.getConfigName()),
+      () -> assertEquals(DEFAULT_MODULE_NAME, actConfig.getModule()),
+      () -> assertEquals(true, actConfig.getDefault()),
+      () -> assertEquals(true, actConfig.getEnabled()),
+      () -> assertEquals(expId, actExportConfig.getId())
     );
   }
 
