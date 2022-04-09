995b004e41655f1bae3572d7d0d0fabf1aba9e78
Andrei <40025074+AndreiMPV@users.noreply.github.com>
2022-04-09 10:29:11 +0200

    [MODEXPS-82] - Add permission to system user (#169)
    (cherry picked from commit 22d6319011807f1f28e2ff6c7a120f7f6b2b9015)

--- src/test/java/org/folio/des/scheduling/acquisition/AcqBaseExportTaskTriggerTest.java
+++ src/test/java/org/folio/des/scheduling/acquisition/AcqBaseExportTaskTriggerTest.java
@@ -16,6 +16,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
