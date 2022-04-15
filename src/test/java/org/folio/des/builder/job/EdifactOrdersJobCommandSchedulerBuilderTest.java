3c0e9393e25d9a52fe283ed5cec8b2ede2be1ed2
Andrei M <Andrei_Makaranka@epam.com>
2022-04-15 09:26:52 +0200

    [MODEXPS-91] - Update tests

--- src/test/java/org/folio/des/builder/job/EdifactOrdersJobCommandSchedulerBuilderTest.java
+++ src/test/java/org/folio/des/builder/job/EdifactOrdersJobCommandSchedulerBuilderTest.java
@@ -1,6 +1,7 @@
 package org.folio.des.builder.job;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
