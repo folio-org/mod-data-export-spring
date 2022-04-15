a0a49371f1bf62a71083d46012b8d93efcf9f7a0
Andrei M <Andrei_Makaranka@epam.com>
2022-04-15 08:43:50 +0200

    [MODEXPS-91] - Duplicates

--- src/main/java/org/folio/des/builder/job/EdifactOrdersJobCommandSchedulerBuilder.java
+++ src/main/java/org/folio/des/builder/job/EdifactOrdersJobCommandSchedulerBuilder.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.folio.de.entity.JobCommand;
-import org.folio.des.domain.dto.Job;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.stereotype.Service;
@@ -22,7 +21,7 @@ public class EdifactOrdersJobCommandSchedulerBuilder implements JobCommandSchedu
   private final ObjectMapper objectMapper;
 
   @Override
-  public JobCommand buildJobCommand(Job job) {
+  public JobCommand buildJobCommand(org.folio.des.domain.dto.Job job) {
     JobCommand jobCommand = buildBaseJobCommand(job);
     Map<String, JobParameter> params = new HashMap<>();
     try {
@@ -36,16 +35,16 @@ public class EdifactOrdersJobCommandSchedulerBuilder implements JobCommandSchedu
     }
   }
 
-  private JobCommand buildBaseJobCommand(Job job) {
+  private JobCommand buildBaseJobCommand(org.folio.des.domain.dto.Job job) {
     var result = new JobCommand();
-    result.setType(JobCommand.Type.START);
     result.setId(job.getId());
-    result.setName(job.getName());
-    result.setDescription(job.getDescription());
     result.setExportType(job.getType());
+    result.setType(JobCommand.Type.START);
     result.setIdentifierType(job.getIdentifierType());
     result.setEntityType(job.getEntityType());
     result.setProgress(job.getProgress());
+    result.setName(job.getName());
+    result.setDescription(job.getDescription());
     return result;
   }
 }
