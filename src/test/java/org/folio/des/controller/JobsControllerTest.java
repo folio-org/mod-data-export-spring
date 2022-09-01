10186c44e89b3b1c73adee8777bfbad647739a19
siarhei-charniak <siarhei_charniak@epam.com>
2022-09-01 16:48:52 +0300

    MODEXPS-141 - Bulk edit: holdings locations - Update submodule (#210)

--- src/test/java/org/folio/des/controller/JobsControllerTest.java
+++ src/test/java/org/folio/des/controller/JobsControllerTest.java
@@ -317,7 +317,11 @@ class JobsControllerTest extends BaseTest {
     "{ \"type\": \"BULK_EDIT_IDENTIFIERS\", \"exportTypeSpecificParameters\" : {}, \"entityType\" : \"ITEM\", \"identifierType\" : \"HRID\"}",
     "{ \"type\": \"BULK_EDIT_IDENTIFIERS\", \"exportTypeSpecificParameters\" : {}, \"entityType\" : \"ITEM\", \"identifierType\" : \"FORMER_IDS\"}",
     "{ \"type\": \"BULK_EDIT_IDENTIFIERS\", \"exportTypeSpecificParameters\" : {}, \"entityType\" : \"ITEM\", \"identifierType\" : \"ACCESSION_NUMBER\"}",
-    "{ \"type\": \"BULK_EDIT_IDENTIFIERS\", \"exportTypeSpecificParameters\" : {}, \"entityType\" : \"ITEM\", \"identifierType\" : \"HOLDINGS_RECORD_ID\"}"
+    "{ \"type\": \"BULK_EDIT_IDENTIFIERS\", \"exportTypeSpecificParameters\" : {}, \"entityType\" : \"ITEM\", \"identifierType\" : \"HOLDINGS_RECORD_ID\"}",
+    "{ \"type\": \"BULK_EDIT_IDENTIFIERS\", \"exportTypeSpecificParameters\" : {}, \"entityType\" : \"HOLDINGS_RECORD\", \"identifierType\" : \"ID\"}",
+    "{ \"type\": \"BULK_EDIT_IDENTIFIERS\", \"exportTypeSpecificParameters\" : {}, \"entityType\" : \"HOLDINGS_RECORD\", \"identifierType\" : \"HRID\"}",
+    "{ \"type\": \"BULK_EDIT_IDENTIFIERS\", \"exportTypeSpecificParameters\" : {}, \"entityType\" : \"HOLDINGS_RECORD\", \"identifierType\" : \"INSTANCE_HRID\"}",
+    "{ \"type\": \"BULK_EDIT_IDENTIFIERS\", \"exportTypeSpecificParameters\" : {}, \"entityType\" : \"HOLDINGS_RECORD\", \"identifierType\" : \"ITEM_BARCODE\"}"
   })
   @DisplayName("Start new bulk edit identifiers job with identifiers and entity type, should be 201")
   void postBulkEditIdentifiersJobWithIdentifiersAndEntityType(String contentString) throws Exception {
