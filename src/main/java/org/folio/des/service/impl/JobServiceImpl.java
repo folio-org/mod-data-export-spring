cbdb0db16f6ae3b04e6a8a1743e3737711b2459a
SinghAdes <90304030+SinghAdes@users.noreply.github.com>
2022-09-13 11:53:36 +0530

    test msg

--- src/main/java/org/folio/des/service/impl/JobServiceImpl.java
+++ src/main/java/org/folio/des/service/impl/JobServiceImpl.java
@@ -104,20 +104,19 @@ public class JobServiceImpl implements JobService {
   @Override
   public org.folio.des.domain.dto.Job upsertAndSendToKafka(org.folio.des.domain.dto.Job jobDto, boolean withJobCommandSend) {
 
-//     Optional.ofNullable(jobDto.getExportTypeSpecificParameters().getVendorEdiOrdersExportConfig().getExportConfigId()
-//     .toString()).ifPresent(
-//       f -> {
-//       try {
-//      log.info("Looking config with id {}", jobDto.getExportTypeSpecificParameters().getVendorEdiOrdersExportConfig().getExportConfigId().toString());
-//      manager.getConfigById(jobDto.getExportTypeSpecificParameters().getVendorEdiOrdersExportConfig().getExportConfigId().toString());
-//    }
-//    catch (NotFoundException e) {
-//      log.info("config not found", jobDto.getExportTypeSpecificParameters().getVendorEdiOrdersExportConfig().getExportConfigId().toString());
-//      throw new NotFoundException(String.format(INTEGRATION_NOT_AVAILABLE, jobDto.getExportTypeSpecificParameters().getVendorEdiOrdersExportConfig().getExportConfigId().toString()));
-//    }
-//
-//        }
-//     );
+     Optional.ofNullable(jobDto.getExportTypeSpecificParameters()).ifPresent(
+       f -> {
+       try {
+      log.info("Looking config with id {}", f.getVendorEdiOrdersExportConfig().getExportConfigId().toString());
+      manager.getConfigById(f.getVendorEdiOrdersExportConfig().getExportConfigId().toString());
+    }
+    catch (NotFoundException e) {
+      log.info("config not found", f.getVendorEdiOrdersExportConfig().getExportConfigId().toString());
+      throw new NotFoundException(String.format(INTEGRATION_NOT_AVAILABLE,f.getVendorEdiOrdersExportConfig().getExportConfigId().toString()));
+    }
+
+        }
+     );
 //      //throw new NotFoundException(String.format(INTEGRATION_NOT_AVAILABLE, jobDto.getExportTypeSpecificParameters().getVendorEdiOrdersExportConfig().getExportConfigId().toString()));
 //
     log.info("Upserting DTO {}.", jobDto);
