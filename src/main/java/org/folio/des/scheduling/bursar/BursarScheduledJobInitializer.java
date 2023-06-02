c53e0be79358e4dc2431e56a2834ca7b3c5a9582
Gurleen Kaur <34331959+gurleenkaurbp@users.noreply.github.com>
2023-06-02 14:38:14 +0530

    Modexps 217  - Migration of existing jobs to quartz on module upgrade for Bursar (#256)
    * [MODEXPS-213] 1. Job , JobDetails , TriggerConverters , JobKeyResolver added 2. Test case added
    
    * [MODEXPS-213] 1. scheduleBursarJob added while updating config
    
    * [MODEXPS-213] 1. logs added
    
    * [MODEXPS-213] 1. current time calculation updated
    
    * [MODEXPS-213] 1. BursarScheduledJobInitializer added in case of postTenant api.
    
    * [MODEXPS-213] 1. Test case added
    
    * [MODEXPS-217] - First set of changes
    
    * [MODEXPS-213] 1. Test case added 2. schdulejob from postConfig 3. deleteJob when no config found while running the scheduler
    
    * [MODEXPS-217] - Second set of changes
    
    * [MODEXPS-213] 1. ExportConfigToBursarTriggerConverter updated with correct Period selected
    
    * [MODEXPS-213] 1. Code restructured
    
    * [MODEXPS-213] 1. Code restructured for postConfig 2. Test cases added / updated
    
    * [MODEXPS-213] 1. Removing old impl. of Bursar Scheduler with test cases and usage 2. Adjusting code and test cases
    
    * [MODEXPS-213] 1. cleanup
    
    * [MODEXPS-217] - Third set of changes
    
    * [MODEXPS-213] 1. reorganised and reformatted code and import
    
    * [MODEXPS-213] 1. changes added according to PR comments pt.1
    
    * [MODEXPS-213] 1. fixing sonar issue
    
    * [MODEXPS-213] 1. changes added according to PR comments pt.2
    
    * [MODEXPS-213] 1. ExportConfigToBursarTriggerConverter NONE case added 2. Test class added
    
    * [MODEXPS-213] 1. trigger is disabled if period is null added
    
    * [MODEXPS-213] 1. Adding back ExportTriggerTest
    
    * [MODEXPS-213] 1. application.yml updated
    
    * [MODEXPS-213] 1. changes added based on PR comments pt.3
    
    * [MODEXPS-213] 1. changes added based on PR comments pt.4
    
    * [MODEXPS-217] - Fourth set of changes
    
    * [MODEXPS-217] - Fifth set of changes
    
    * [MODEXPS-217] - Sixth set of changes
    
    * [MODEXPS-217] - Seventh set of changes
    
    * [MODEXPS-217] - Eight set of changes
    
    ---------
    
    Co-authored-by: Arghya22 <Arghya_Mitra@epam.com>
    Co-authored-by: Arghya Mitra <103935659+Arghya22@users.noreply.github.com>

--- src/main/java/org/folio/des/scheduling/bursar/BursarScheduledJobInitializer.java
+++ src/main/java/org/folio/des/scheduling/bursar/BursarScheduledJobInitializer.java
@@ -1,10 +1,14 @@
 package org.folio.des.scheduling.bursar;
 
+import static org.folio.des.scheduling.acquisition.ScheduleUtil.shouldMigrateSchedulesToQuartz;
+
 import java.util.Optional;
 
 import org.folio.des.domain.dto.ExportConfig;
 import org.folio.des.scheduling.BursarExportScheduler;
 import org.folio.des.service.config.ExportConfigService;
+import org.folio.okapi.common.SemVer;
+import org.folio.tenant.domain.dto.TenantAttributes;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
@@ -18,14 +22,19 @@ public class BursarScheduledJobInitializer {
   private final ExportConfigService burSarExportConfigService;
   private final BursarExportScheduler bursarExportScheduler;
 
-  public void initAllScheduledJob() {
+
+  private final SemVer quartzBursarMinVersion = new SemVer("3.0.0-SNAPSHOT");
+
+  public void initAllScheduledJob(TenantAttributes tenantAttributes) {
     log.info("initAllScheduledJob:: initiating scheduled job of type Bursar");
     try {
-      Optional<ExportConfig> savedConfig = burSarExportConfigService.getFirstConfig();
-      if (savedConfig.isPresent()) {
-        bursarExportScheduler.scheduleBursarJob(savedConfig.get());
-      } else {
-        log.info("initAllScheduledJob:: No export schedules found.");
+      if (shouldMigrateSchedulesToQuartz(tenantAttributes, quartzBursarMinVersion)) {
+        Optional<ExportConfig> savedConfig = burSarExportConfigService.getFirstConfig();
+        if (savedConfig.isPresent()) {
+          bursarExportScheduler.scheduleBursarJob(savedConfig.get());
+        } else {
+          log.info("initAllScheduledJob:: No export schedules found.");
+        }
       }
     } catch (Exception e) {
       log.warn("scheduling failure", e);
