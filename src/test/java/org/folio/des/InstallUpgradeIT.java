2cb113fcfd2537f26a9da755f46468ef0d537b80
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-07-17 19:59:48 +0200

    Fix typo in env variable name

--- src/test/java/org/folio/des/InstallUpgradeIT.java
+++ src/test/java/org/folio/des/InstallUpgradeIT.java
@@ -95,7 +95,7 @@ class InstallUpgradeIT {
     .withEnv("DB_DATABASE", "postgres")
     .withEnv("KAFKA_HOST", "mykafka")
     .withEnv("KAFKA_PORT", "9092")
-    .withEnv("SYSTEM_USER_PASSWORD ", "password");
+    .withEnv("SYSTEM_USER_PASSWORD", "password");
 
   private static void mockPath(MockServerClient mockServerClient, String path, String jsonBody) {
     mockServerClient.when(request(path))
