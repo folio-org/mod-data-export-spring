67f2af55ebda22e0551b199d123d5a937350207e
Dmytro_Bykov <dmytro_bykov@epam.com>
2024-03-13 21:36:54 +0500

    MODEXPS-253: registry.removeBeanDefinition("jobRegistryBeanPostProcessor")

--- src/main/java/org/folio/des/config/scheduling/QuartzSchedulerFactoryBeanCustomizer.java
+++ src/main/java/org/folio/des/config/scheduling/QuartzSchedulerFactoryBeanCustomizer.java
@@ -1,6 +1,5 @@
 package org.folio.des.config.scheduling;
 
-import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.config.DataSourceFolioWrapper;
 import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
@@ -8,23 +7,16 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
-import javax.sql.DataSource;
-import java.util.Objects;
-
 @Component
 @RequiredArgsConstructor
 public class QuartzSchedulerFactoryBeanCustomizer implements SchedulerFactoryBeanCustomizer {
-
-  private final DataSource dataSource;
-  private final FolioExecutionContext folioExecutionContext;
-
+  private final DataSourceFolioWrapper dataSourceFolioWrapper;
 
   /**
    * Use plain datasource instead of folio wrapper for quartz since it does not need tenant specific schemas logic
    */
   @Override
   public void customize(SchedulerFactoryBean schedulerFactoryBean) {
-    DataSourceFolioWrapper wrapper = new DataSourceFolioWrapper(dataSource, folioExecutionContext);
-    schedulerFactoryBean.setDataSource(Objects.requireNonNull(wrapper.getTargetDataSource()));
+    schedulerFactoryBean.setDataSource(dataSourceFolioWrapper.getTargetDataSource());
   }
 }
