73bf75cd1903cd9a72836f19e3e89b9f6423f8c1
Dmytro_Bykov <dmytro_bykov@epam.com>
2024-03-14 19:57:24 +0500

    MODEXPS-253: QuartzSchedulerFactoryBeanCustomizer back to wrapper usage

--- src/main/java/org/folio/des/config/scheduling/QuartzSchedulerFactoryBeanCustomizer.java
+++ src/main/java/org/folio/des/config/scheduling/QuartzSchedulerFactoryBeanCustomizer.java
@@ -1,23 +1,21 @@
 package org.folio.des.config.scheduling;
 
+import org.folio.spring.config.DataSourceFolioWrapper;
 import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
-import javax.sql.DataSource;
-
 @Component
 @RequiredArgsConstructor
 public class QuartzSchedulerFactoryBeanCustomizer implements SchedulerFactoryBeanCustomizer {
-  private final DataSource dataSource;
+  private final DataSourceFolioWrapper dataSourceFolioWrapper;
 
   /**
    * Use plain datasource instead of folio wrapper for quartz since it does not need tenant specific schemas logic
    */
   @Override
   public void customize(SchedulerFactoryBean schedulerFactoryBean) {
-    schedulerFactoryBean.setDataSource(dataSource);
-  }
+    schedulerFactoryBean.setDataSource(dataSourceFolioWrapper.getTargetDataSource());  }
 }
