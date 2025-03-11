ed66d83483d829e1f21308f0b0a5ae7544628297
saba_zedginidze <saba_zedginidze@epam.com>
2025-03-12 00:30:13 +0400

    [MODEXPS-286] Escape JDBC ? with double ??

--- src/main/resources/db/changelog/changes/2025_10_03_populate_orders_jobs_missing_fields.sql
+++ src/main/resources/db/changelog/changes/2025_10_03_populate_orders_jobs_missing_fields.sql
@@ -5,7 +5,7 @@ SET export_type_specific_parameters = jsonb_set(export_type_specific_parameters,
     'transmissionMethod', 'FTP',
     'fileFormat', 'EDI'
   ), true)
-WHERE type = 'EDIFACT_ORDERS_EXPORT' AND export_type_specific_parameters ? 'vendorEdiOrdersExportConfig'
-  AND NOT (export_type_specific_parameters->'vendorEdiOrdersExportConfig' ? 'integrationType')
-  AND NOT (export_type_specific_parameters->'vendorEdiOrdersExportConfig' ? 'transmissionMethod')
-  AND NOT (export_type_specific_parameters->'vendorEdiOrdersExportConfig' ? 'fileFormat');
+WHERE type = 'EDIFACT_ORDERS_EXPORT' AND export_type_specific_parameters ?? 'vendorEdiOrdersExportConfig'
+  AND NOT (export_type_specific_parameters->'vendorEdiOrdersExportConfig' ?? 'integrationType')
+  AND NOT (export_type_specific_parameters->'vendorEdiOrdersExportConfig' ?? 'transmissionMethod')
+  AND NOT (export_type_specific_parameters->'vendorEdiOrdersExportConfig' ?? 'fileFormat');
