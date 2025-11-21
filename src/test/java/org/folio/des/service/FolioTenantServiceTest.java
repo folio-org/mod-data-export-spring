3bf1ce142b61e7826c987b9e0d0ee602a4cbe186
Serhii_Nosko <Serhii_Nosko@epam.com>
2025-11-21 12:52:35 +0200

    Revert "MODEXPW-611. Assign permissions for the system user only from ModuleDescriptor-template.json (#357)"
    This reverts commit ed0e40e20691f097acd0e3d534a85995b01fcd95.

--- src/test/java/org/folio/des/service/FolioTenantServiceTest.java
+++ src/test/java/org/folio/des/service/FolioTenantServiceTest.java
@@ -14,6 +14,7 @@ import org.folio.des.scheduling.quartz.OldJobDeleteScheduler;
 import org.folio.des.scheduling.quartz.ScheduledJobsRemover;
 import org.folio.des.service.bursarlegacy.BursarMigrationService;
 import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.service.PrepareSystemUserService;
 import org.folio.tenant.domain.dto.TenantAttributes;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -46,12 +47,17 @@ class FolioTenantServiceTest {
   OldJobDeleteScheduler oldJobDeleteScheduler;
 
   @Mock
+  PrepareSystemUserService prepareSystemUserService;
+
+  @Mock
   BursarMigrationService bursarMigrationService;
 
   @Test
   void shouldDoProcessAfterTenantUpdating() {
     TenantAttributes tenantAttributes = createTenantAttributes();
 
+    doNothing().when(prepareSystemUserService)
+      .setupSystemUser();
     doNothing().when(edifactScheduledJobInitializer)
       .initAllScheduledJob(tenantAttributes);
     doNothing().when(kafka)
@@ -67,6 +73,7 @@ class FolioTenantServiceTest {
 
     folioTenantService.afterTenantUpdate(tenantAttributes);
 
+    verify(prepareSystemUserService).setupSystemUser();
     verify(edifactScheduledJobInitializer, times(1)).initAllScheduledJob(tenantAttributes);
     verify(bursarScheduledJobInitializer, times(1)).initAllScheduledJob(tenantAttributes);
     verify(oldJobDeleteScheduler, times(1)).scheduleOldJobDeletion(any());
